b4716383596088b975c3cd7f9b25ea8b5bd8ae47
Leeeuswa <leeco6578@naver.com>
2026-04-16 17:22:11 +0900

    댓글 수정 삭제

--- src/main/java/com/example/b01/dto/ReplyDTO.java
+++ src/main/java/com/example/b01/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package com.example.b01.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -22,6 +24,8 @@ public class ReplyDTO {
     private String replyText;
     @NotEmpty
     private String replyer;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime regDate;
+    @JsonIgnore
     private LocalDateTime modDate;
 }
--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -10,5 +10,30 @@ async function get1(bno) { // 함수 내부에서 await 사용 가능 . 항상 P
 
 async function getList({bno, page, size, goLast}){
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
     return result.data
+}
+
+async function addReply(replyObj){
+    const response = await axios.post(`/replies/`,replyObj)
+    return response.data
+}
+
+async function getReply(rno) {
+    const response = await axios.get(`/replies/${rno}`)
+    return response.data
+}
+
+async  function modifyReply(replyObj){
+    const response = await axios.put(`/replies/${replyObj.rno}`,replyObj)
+    return response.data
+}
+
+async function removeReply(rno) {
+    const response = await axios.delete(`/replies/${rno}`)
+    return response.data
 }
